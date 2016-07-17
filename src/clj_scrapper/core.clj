a5649d1961c0270e51a944b8e7b52d4ccd637484
Burin Choomnuan <agilecreativity@gmail.com>
2016-07-17 14:28:09 -0400

    Initial commit

---
+++ src/clj_scrapper/core.clj
@@ -0,0 +1,6 @@
+(ns clj-scrapper.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_scrapper/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-scrapper.core-test
+  (:require [clojure.test :refer :all]
+            [clj-scrapper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
